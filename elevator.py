ead07807b70ea45fcbcd22d5b6cf898d39285052
Max Hausch <cheriimoya@googlemail.com>
2019-04-16 19:12:32 +0200

    Add exeptions

--- elevator.py
+++ elevator.py
@@ -1,10 +1,22 @@
 class Elevator:
-    def __init__(self, floor):
+    def __init__(self, elevator_id, floor=0, capacity=10):
+        if elevator_id is not int:
+            raise TypeError('elevator_id has to be an int')
+
+        if self.floor not < Environment.FLOOR:
+            raise ValueError('Floor too high')
+
+        self.elevator_id = elevator_id
         self.floor = floor
 
-    def goUp(self):
+    def go_up(self):
+        if self.floor not < Environment.FLOOR
+            raise ValueError('Floor too high')
+
         self.floor += 1
 
-    def goDown(self):
-        if(self.floor > 0): 
-            self.floor -= 1
+    def go_down(self):
+        if self.floor == 0 :
+            raise ValueError('Elevator is on floor 0')
+
+        self.floor -= 1
--- environment.py
+++ environment.py
@@ -2,6 +2,9 @@ import math
 import numpy as np
 
 class Environment:
+    FLOORS = 6
+    ELEVATORS = 3
+
     def __init__(self):
         self.floors = np.arange(7)
         self.waitingPeople = np.zeros(7) 
