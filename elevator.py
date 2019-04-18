96c7c85769f4d04d4c8a7f7f73cbb76057cd20b2
Karim Dhifallah <Karim_Dhifallah@hotmail.com>
2019-04-18 20:48:41 +0200

    Implemented DirectionState

--- elevator.py
+++ elevator.py
@@ -1,26 +1,45 @@
 from environment import Environment
+from enum import Enum
+
+class DirectionState(Enum):
+    IDLE = 0
+    UP = 1
+    DOWN = 2
 
 class Elevator:
-    def __init__(self, elevator_id, floor=0, capacity=10):
+    def __init__(self, elevator_id, current_floor=0, capacity=10):
         if type(elevator_id) is not int:
             raise TypeError('elevator_id has to be an int')
 
-        if not floor < Environment.FLOORS:
+        if not current_floor < Environment.FLOORS:
             raise ValueError('Floor too high')
 
         self.elevator_id = elevator_id
-        self.floor = floor
+        self.current_floor = current_floor
+        self.destination_floor = None
         self.capacity = capacity
         self.people_in_elevator = 0
+        
 
     def go_up(self):
-        if not self.floor < Environment.FLOORS:
+        if not self.current_floor < Environment.FLOORS:
             raise ValueError('Floor too high')
 
-        self.floor += 1
+        self.current_floor += 1
 
     def go_down(self):
-        if self.floor == 0:
+        if self.current_floor == 0:
             raise ValueError('Elevator is on floor 0')
 
-        self.floor -= 1
+        self.current_floor -= 1
+        
+    def getDirection(self):
+        dir = self.destination_floor - self.current_floor
+
+        if dir < 0:
+            return DirectionState.DOWN
+        elif dir > 0:
+            return DirectionState.UP
+        else:
+            return DirectionState.IDLE
+        
