b37d0b74846c15325631c17a9cc22f3464090978
Alexander Belokon <alexander.belokon.mail@gmail.com>
2019-06-20 22:30:13 +0200

    ready for training

--- gym_environment.py
+++ gym_environment.py
@@ -3,6 +3,7 @@ import gym
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
+import copy
 
 import pyglet
 from pyglet import gl
@@ -38,21 +39,84 @@ class ElevatorEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, action):
+    def eval(self, action):
         assert self.action_space.contains(
             action), "%r (%s) invalid" % (action, type(action))
-        state = self.state
-
+        state = copy.copy(self.state)
+        current_floor = int(state[0])
+        stockwerk_index = ((current_floor-1) * self.floor_limit) + \
+            1 + self.elevator_limit
         reward = 0
         if action == 0:
             if state[0] == 1:
-                reward -= 100
+                reward -= 1000000
             else:
+                # check if there is reason to go a floor up
+                # is there a passenger waiting at a lower a floor?
+                passenger_at_lower_floors = False
+                for i in range(1, current_floor):
+                    start = ((current_floor-1) * self.floor_limit) + \
+                        1 + self.elevator_limit
+                    for k in range(0, self.floor_limit):
+                        if state[start+k] > 0:
+                            passenger_at_lower_floors = True
+                        if passenger_at_lower_floors:
+                            break
+                    if passenger_at_lower_floors:
+                        break
+
+                # is a passanger inside the elevator destinated to an upper floor?
+                passenger_destinated_to_lower_floors = False
+                for i in range(1, self.elevator_limit+1):
+                    if state[i] < current_floor:
+                        passenger_destinated_to_lower_floors = True
+                        break
+
+                # shit action m8
+                if not passenger_at_lower_floors and not passenger_destinated_to_lower_floors:
+                    reward -= 10
+
+                for k in range(0, self.floor_limit):
+                    if self.state[stockwerk_index + k] != 0:
+                        reward -= 10
+                        break
+
                 state[0] -= 1
         elif action == 1:
             if state[0] == self.floor_num:
-                reward -= 100
+                reward -= 1000000
             else:
+                # check if there is reason to go a floor up
+
+                # is there a passenger waiting at a higher a floor?
+                passenger_at_upper_floors = False
+                for i in range(current_floor+1, self.floor_num+1):
+                    start = ((current_floor-1) * self.floor_limit) + \
+                        1 + self.elevator_limit
+                    for k in range(0, self.floor_limit):
+                        if state[start+k] > 0:
+                            passenger_at_upper_floors = True
+                        if passenger_at_upper_floors:
+                            break
+                    if passenger_at_upper_floors:
+                        break
+
+                # is a passanger inside the elevator destinated to an upper floor?
+                passenger_destinated_to_upper_floors = False
+                for i in range(1, self.elevator_limit+1):
+                    if state[i] > current_floor:
+                        passenger_destinated_to_upper_floors = True
+                        break
+
+                # shit action m8
+                if not passenger_at_upper_floors and not passenger_destinated_to_upper_floors:
+                    reward -= 10
+
+                for k in range(0, self.floor_limit):
+                    if self.state[stockwerk_index + k] != 0:
+                        reward -= 10
+                        break
+
                 state[0] += 1
         elif action == 2:
             current_floor = state[0]
@@ -66,7 +130,7 @@ class ElevatorEnv(gym.Env):
                     continue
                 if state[i] == current_floor:
                     state[i] = 0
-                    reward += 10
+                    reward += 100
                     self.waiting_passangers -= 1
                     passengers_left = True
                 else:
@@ -75,10 +139,144 @@ class ElevatorEnv(gym.Env):
 
             # indices to iterate over for passengers waiting on the current floor
             # state[i] equals passenger destination
-            start = (current_floor * self.floor_limit) + 1
+            start = ((current_floor-1) * self.floor_limit) + \
+                1 + self.elevator_limit
             stop = start + self.floor_limit
             for i in range(int(start), int(stop)):
-                print(i)
+
+                if state[i] == 0:
+                    continue
+
+                # if elevator capacity is at max
+                if passengers_in_elevator_num == self.elevator_limit:
+                    reward = -10
+
+                for j in range(
+                        int(start),
+                        int(stop)):
+                    for k in range(1, self.elevator_limit):
+                        if state[k] == 0:
+                            # move passenger into elevator
+                            state[k] = state[j]
+                            state[j] = 0
+                            passengers_entered = True
+
+            if passengers_entered == False and passengers_left == False:
+                reward = -2
+
+        if reward == 0:
+            reward = -1
+
+        done = False
+        if self.waiting_passangers == 0:
+            done = True
+            reward += 400
+        return np.array(state), reward, done, {}
+
+    def step(self, action):
+        assert self.action_space.contains(
+            action), "%r (%s) invalid" % (action, type(action))
+        state = self.state
+        current_floor = int(state[0])
+        stockwerk_index = ((current_floor-1) * self.floor_limit) + \
+            1 + self.elevator_limit
+        reward = 0
+        if action == 0:
+            if state[0] == 1:
+                reward -= 1000000
+            else:
+                # check if there is reason to go a floor up
+
+                # is there a passenger waiting at a lower a floor?
+                passenger_at_lower_floors = False
+                for i in range(1, current_floor):
+                    start = ((current_floor-1) * self.floor_limit) + \
+                        1 + self.elevator_limit
+                    for k in range(0, self.floor_limit):
+                        if state[start+k] > 0:
+                            passenger_at_lower_floors = True
+                        if passenger_at_lower_floors:
+                            break
+                    if passenger_at_lower_floors:
+                        break
+
+                # is a passanger inside the elevator destinated to an upper floor?
+                passenger_destinated_to_lower_floors = False
+                for i in range(1, self.elevator_limit+1):
+                    if state[i] < current_floor:
+                        passenger_destinated_to_lower_floors = True
+                        break
+
+                # shit action m8
+                if not passenger_at_lower_floors and not passenger_destinated_to_lower_floors:
+                    reward -= 10
+
+                for k in range(0, self.floor_limit):
+                    if self.state[stockwerk_index + k] != 0:
+                        reward -= 10
+                        break
+
+                state[0] -= 1
+        elif action == 1:
+            if state[0] == self.floor_num:
+                reward -= 1000000
+            else:
+                # check if there is reason to go a floor up
+
+                # is there a passenger waiting at a higher a floor?
+                passenger_at_upper_floors = False
+                for i in range(current_floor+1, self.floor_num+1):
+                    start = ((current_floor-1) * self.floor_limit) + \
+                        1 + self.elevator_limit
+                    for k in range(0, self.floor_limit):
+                        if state[start+k] > 0:
+                            passenger_at_upper_floors = True
+                        if passenger_at_upper_floors:
+                            break
+                    if passenger_at_upper_floors:
+                        break
+
+                # is a passanger inside the elevator destinated to an upper floor?
+                passenger_destinated_to_upper_floors = False
+                for i in range(1, self.elevator_limit+1):
+                    if state[i] > current_floor:
+                        passenger_destinated_to_upper_floors = True
+
+                # shit action m8
+                if not passenger_at_upper_floors and not passenger_destinated_to_upper_floors:
+                    reward -= 10
+
+                for k in range(0, self.floor_limit):
+                    if self.state[stockwerk_index + k] != 0:
+                        reward -= 10
+                        break
+
+                state[0] += 1
+        elif action == 2:
+            passengers_in_elevator_num = 0
+            passengers_entered = False
+            passengers_left = False
+            # indices to iterate over for passengers in the elevator
+            # state[i] equals passenger destination
+            for i in range(1, self.elevator_limit + 1):
+                if state[i] == 0:
+                    continue
+                if state[i] == current_floor:
+                    state[i] = 0
+                    reward += 100
+                    self.waiting_passangers -= 1
+                    passengers_left = True
+                else:
+                    passengers_in_elevator_num += 1
+            # passengers_in_elevator_num = amount of passengers that did not leave elevator
+
+            # indices to iterate over for passengers waiting on the current floor
+            # state[i] equals passenger destination
+            start = ((current_floor-1) * self.floor_limit) + \
+                1 + self.elevator_limit
+            stop = start + self.floor_limit
+            for i in range(int(start), int(stop)):
+
                 if state[i] == 0:
                     continue
 
@@ -118,85 +316,88 @@ class ElevatorEnv(gym.Env):
         self.state[0] = 1
 
         # here index is 51
-        # self.state[1 + self.elevator_limit +
-        #           (self.floor_num - 1) * self.floor_limit] = 1
+        self.waiting_passangers = 20
+        for i in range(self.waiting_passangers):
 
-        random_floor = int(self.np_random.uniform(1, self.floor_num + 1))
-        random_destination = int(self.np_random.uniform(1, self.floor_num + 1))
+            # self.state[1 + self.elevator_limit +
+            #         (self.floor_num - 1) * self.floor_limit] = 1
 
-        while random_floor == random_destination:
+            random_floor = int(self.np_random.uniform(1, self.floor_num + 1))
             random_destination = int(
                 self.np_random.uniform(1, self.floor_num + 1))
 
-        self.state[int(1 + self.elevator_limit + (random_floor - 1) *
-                       self.floor_limit)] = random_destination
+            while random_floor == random_destination:
+                random_destination = int(
+                    self.np_random.uniform(1, self.floor_num + 1))
 
-        self.waiting_passangers = 1
+            stockwerk_index = int(1 + self.elevator_limit + ((random_floor - 1) *
+                                                             self.floor_limit))
+            for k in range(0,  self.floor_limit):
+                if self.state[stockwerk_index+k] == 0:
+                    self.state[stockwerk_index+k] = random_destination
+                    break
         self.steps_beyond_done = None
         return np.array(self.state)
 
     def render(self, mode='human'):
+        from gym.envs.classic_control import rendering
         self.screen_width = 600
         self.screen_height = 400
 
         if self.viewer is None:
-            from gym.envs.classic_control import rendering
+
             self.viewer = rendering.Viewer(self.screen_width,
                                            self.screen_height)
-            self.transform = rendering.Transform()
+        self.transform = rendering.Transform()
 
-            self.floor_padding = (self.screen_height - 100) / self.floor_num
-            boxwidth = self.floor_padding / 1.5
-            boxheight = self.floor_padding
-            l, r, t, b = -boxwidth / 2, boxwidth / 2, boxheight - boxwidth / 2, -boxwidth / 2
-            box = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            self.boxtrans = rendering.Transform(
-                (self.screen_width / 2 + boxwidth,
-                 (self.state[0] * self.floor_padding - 30) + 40))
-            box.add_attr(self.boxtrans)
-            box.set_color(.4, .4, .4)
+        self.floor_padding = (self.screen_height - 100) / self.floor_num
+        boxwidth = self.floor_padding / 1.5
+        boxheight = self.floor_padding
+        l, r, t, b = -boxwidth / 2, boxwidth / 2, boxheight - boxwidth / 2, -boxwidth / 2
+        box = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+        self.boxtrans = rendering.Transform(
+            (self.screen_width / 2 + boxwidth,
+                (self.state[0] * self.floor_padding - 30) + 40))
+        box.add_attr(self.boxtrans)
+        box.set_color(.4, .4, .4)
 
-            for i in range(self.floor_num):
-                start = self.floor_num * (i + 1) + self.elevator_limit
-                stop = start + self.floor_limit
+        for i in range(self.floor_num):
+            start = self.floor_num * (i + 1) + self.elevator_limit
+            stop = start + self.floor_limit
 
-                for j in range(int(start), int(stop)):
-                    if j != 0:
-                        print('ok')
+        self.viewer.add_geom(box)
 
-            self.viewer.add_geom(box)
+        win = self.viewer.window
+        win.switch_to()
+        win.dispatch_events()
 
-            win = self.viewer.window
-            win.switch_to()
-            win.dispatch_events()
+        win.clear()
+        t = self.transform
+        self.score_label = pyglet.text.Label('HELLO WORLD',
+                                             font_size=36,
+                                             x=20,
+                                             y=self.screen_height * 2.5 /
+                                             40.00,
+                                             anchor_x='left',
+                                             anchor_y='center',
+                                             color=(0, 0, 0, 255))
+        pixel_scale = 1
+        if hasattr(win.context, '_nscontext'):
+            pixel_scale = win.context._nscontext.view().backingScaleFactor(
+            )  # pylint: disable=protected-access
+        VP_W = int(pixel_scale * self.screen_width)
+        VP_H = int(pixel_scale * self.screen_height)
 
-            win.clear()
-            t = self.transform
-            self.score_label = pyglet.text.Label('HELLO WORLD',
-                                                 font_size=36,
-                                                 x=20,
-                                                 y=self.screen_height * 2.5 /
-                                                 40.00,
-                                                 anchor_x='left',
-                                                 anchor_y='center',
-                                                 color=(0, 0, 0, 255))
-            pixel_scale = 1
-            if hasattr(win.context, '_nscontext'):
-                pixel_scale = win.context._nscontext.view().backingScaleFactor(
-                )  # pylint: disable=protected-access
-            VP_W = int(pixel_scale * self.screen_width)
-            VP_H = int(pixel_scale * self.screen_height)
+        gl.glViewport(0, 0, VP_W, VP_H)
 
-            gl.glViewport(0, 0, VP_W, VP_H)
+        t.enable()
+        self.render_floors()
+        self.render_indicators(self.screen_width, self.screen_height)
+        self.render_elevators()
+        t.disable()
 
-            t.enable()
-            self.render_floors()
-            self.render_indicators(self.screen_width, self.screen_height)
-            self.render_elevators()
-            t.disable()
-
-            win.flip()
-            return self.viewer.isopen
+        win.flip()
+        return self.viewer.isopen
 
     def render_floors(self):
         PLAYFIELD = 2000
